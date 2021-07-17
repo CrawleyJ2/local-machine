b6f227ce861d5e250db3a6e37ec9f933378d6b10
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-07-17 15:00:30 -0400

    models and index setup

---
+++ models/index.js
@@ -0,0 +1,8 @@
+const User = require ('./User');
+const Post = require ('./Post');
+const Group = require ('./Group');
+const Comment = require ('./Comment');
+
+// relationships
+
+module.exports = { User, Post, Group, Comment };
