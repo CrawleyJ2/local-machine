e57dbf684c9eec5c11b410bf1422aaa9f69f8567
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-07-19 19:13:17 -0400

    updates to models and index

--- models/index.js
+++ models/index.js
@@ -7,23 +7,46 @@ const Location = require ('./Location');
 // Model relationships
 
 // User belongs to one location
-
+User.belongsTo(Location, {
+    foreignKey: 'location_id',
+})
 // Location can have many users
+Location.hasMany(User, {
 
+})
 // Location can have many groups
+Location.hasMany(Group, {
 
+})
 // User has many posts
-
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
 // Posts belongs to User
-
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
 // User belongs to post through comments
-
+User.belongsToMany(Post, {
+    through: Comment,
+    foreignKey: 'user_id'
+})
 // User belongs to group(s)
-
+User.belongsToMany(Group, {
+    
+})
 // User has many Comments
-
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
 // Post has many Comments
-
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
 // Group has many posts
+Group.hasMany(Post, {
 
+})
 module.exports = { User, Post, Group, Comment, Location };
