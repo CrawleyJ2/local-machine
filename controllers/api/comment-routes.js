5bde6d2b6d88b3093a2ccf52ae5004bd33d8575f
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-07-20 21:45:58 -0400

    routing and updates to index

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,13 +1,45 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
-const  = require();
+
 router.get('/', (req, res) => {
-    Comment.findAll({})
-        .then(=> res.json())
+    Comment.findAll()
+        .then(dbCommentData => res.json(dbCommentData))
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
-        })
+        });
 });
 
-router.get('/:id', (req, res) =>
+router.post('/', (req, res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.session.user_id,
+        post_id: req.body.post_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCommentData => {
+        if (!dbCommentData) {
+            res.status(404).json({ message: 'No comment found with that id'});
+            return;
+        }
+        res.json(dbCommentData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
---
+++ controllers/api/group-routes.js
@@ -0,0 +1,102 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { Group, User } = require('../../models');
+
+router.get('/', (req, res) => {
+    Group.findAll({
+        attributes: [
+            'id',
+            'group_name',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbGroupData => res.json(dbGroupData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    Group.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbGroupData => res.json(dbGroupData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', (req,res) => {
+    Group.create({
+        group_name: req.body.group_name,
+        user_id: req.session.user_id
+    })
+    .then(dbGroupData => res.json(dbGroupData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.put('/:id', (req, res) =>{
+    Group.update(
+        {
+            group_name: req.body.group_name
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(dbGroupData => {
+        if (!dbGroupData) {
+            res.status(404).json({ message: 'No group found with this ID' });
+            return;
+        }
+        res.json(dbGroupData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    console.log('id', req.params.id);
+    Group.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbGroupData => {
+        if (!dbGroupData) {
+            res.status(404).json({ message: 'No group found with this ID' });
+            return;
+        }
+        res.json(dbGroupData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
