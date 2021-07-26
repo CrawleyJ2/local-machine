ec055d5d441acb1f22ec0d28109037df9592f5b7
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-07-25 23:02:55 -0400

    reverting to original login route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -72,11 +72,39 @@ router.post("/", (req, res) => {
 });
   
 // login route
-router.post('/login/password', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  failureMessage: true
-}));
+router.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user found!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    req.session.save(() => {
+      // declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      console.log(req.session, 'api login');
+      return res.status(200).json({ user: dbUserData, message: "You are now logged in!" });
+    });
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
   
 // logout route
 router.post("/logout", (req, res) => {
